29a0e9803535eda30bb51a5229def9f2d901fe64
Giuliano Geiss Arnhold <giuliarnhold@gmail.com>
2023-09-24 14:37:48 -0300

    fix: adjust the logic with play/pause button and convolution switch button

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -96,8 +96,22 @@
                         audioElement.play();
                         playButton.dataset.playing = 'true';
                         playButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-8 h-8"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" /></svg>`
+                        track.disconnect();
 
-                        track.connect(analyser).connect(audioContext.destination);
+
+                        if (convolutionButton.checked) {
+                            $url = document.getElementById('impulse').value;
+                            if ($url != '') {
+                                room = await convolve($url);
+                                track.disconnect();
+                                track.connect(room).connect(analyser).connect(audioContext.destination);
+                            } else {
+                                convolutionButton.checked = false;
+                            }
+                        } else {
+                            track.disconnect();
+                            track.connect(analyser).connect(audioContext.destination);
+                        }
 
                         analyser.getFloatFrequencyData(dataArray);
 
@@ -128,7 +142,7 @@
                         convolutionButton.checked = false;
                     }
                 } else {
-                    room.disconnect();
+                    track.disconnect();
                     track.connect(analyser).connect(audioContext.destination);
                 }
                 }
